e4f5497d0bf5b59346403c161616fcbcefbbb1e1
Katherinne <katherinne.v.valladares@gmail.com>
2020-12-13 22:05:00 -0600

    My first excercise

---
+++ SquareNumber.java
@@ -0,0 +1,25 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class SquareNumber {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+
+		double a, b, result;
+
+		System.out.print("digite el valor de a: ");
+		a = reader.nextDouble();
+
+		System.out.print("digite el valor de b: ");
+		b = reader.nextDouble();
+
+		result = Math.pow(a, 2) + Math.pow(b, 2) + (2 * a * b);
+
+		System.out.print("El resultado es: " + result);
+
+	}
+
+}
+
