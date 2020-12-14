b39337819f55c00e12ee44ff0dad0ceb3efc8df4
Katherinne <katherinne.v.valladares@gmail.com>
2020-12-13 22:10:05 -0600

    Adding messages to the program

--- SquareNumber.java
+++ SquareNumber.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class SquareNumber {
 
 	public static void main(String[] args) {
+		System.out.print("Inicio del programa");
 		Scanner reader = new Scanner(System.in);
 
 		double a, b, result;
@@ -18,6 +19,7 @@ public class SquareNumber {
 		result = Math.pow(a, 2) + Math.pow(b, 2) + (2 * a * b);
 
 		System.out.print("El resultado es: " + result);
+		System.out.print("Fin del programa");
 
 	}
 
